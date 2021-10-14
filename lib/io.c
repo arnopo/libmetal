d3b2a2ceb1ea71027c022a260d619e7e1aebf71a
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2021-10-14 17:29:10 +0200

    io: temporary workaround
    Defining metal_io_write & metal_io_read in io.c result in a zephyr
    linker errorµ. Seems linked to the atomic libray
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

--- lib/io.c
+++ lib/io.c
@@ -9,6 +9,10 @@
 #include <metal/io.h>
 #include <metal/sys.h>
 
+extern uint64_t metal_io_read(struct metal_io_region *io, unsigned long offset,
+		       memory_order order, int width);
+extern void metal_io_write(struct metal_io_region *io, unsigned long offset,
+		    uint64_t value, memory_order order, int width);
 /**
  * @brief	Close a libmetal shared memory segment.
  * @param[in]	io	I/O region handle.
@@ -123,77 +127,6 @@ void *metal_io_phys_to_virt(struct metal_io_region *io, metal_phys_addr_t phys)
 metal_phys_addr_t metal_io_virt_to_phys(struct metal_io_region *io, void *virt)
 {
 	return metal_io_phys(io, metal_io_virt_to_offset(io, virt));
-}
-
-/**
- * @brief	Read a value from an I/O region.
- * @param[in]	io	I/O region handle.
- * @param[in]	offset	Offset into I/O region.
- * @param[in]	order	Memory ordering.
- * @param[in]	width	Width in bytes of datatype to read.  This must be 1, 2,
- *			4, or 8, and a compile time constant for this function
- *			to inline cleanly.
- * @return	Value.
- */
-uint64_t metal_io_read(struct metal_io_region *io, unsigned long offset,
-		       memory_order order, int width)
-{
-	void *ptr = metal_io_virt(io, offset);
-
-	if (io->ops.read)
-		return (*io->ops.read)(io, offset, order, width);
-	else if (ptr && sizeof(atomic_uchar) == width)
-		return atomic_load_explicit((atomic_uchar *)ptr, order);
-	else if (ptr && sizeof(atomic_ushort) == width)
-		return atomic_load_explicit((atomic_ushort *)ptr, order);
-	else if (ptr && sizeof(atomic_uint) == width)
-		return atomic_load_explicit((atomic_uint *)ptr, order);
-	else if (ptr && sizeof(atomic_ulong) == width)
-		return atomic_load_explicit((atomic_ulong *)ptr, order);
-#ifndef NO_ATOMIC_64_SUPPORT
-	else if (ptr && sizeof(atomic_ullong) == width)
-		return atomic_load_explicit((atomic_ullong *)ptr, order);
-#endif
-	metal_assert(0);
-	return 0; /* quiet compiler */
-}
-
-/**
- * @brief	Write a value into an I/O region.
- * @param[in]	io	I/O region handle.
- * @param[in]	offset	Offset into I/O region.
- * @param[in]	value	Value to write.
- * @param[in]	order	Memory ordering.
- * @param[in]	width	Width in bytes of datatype to read.  This must be 1, 2,
- *			4, or 8, and a compile time constant for this function
- *			to inline cleanly.
- */
-void metal_io_write(struct metal_io_region *io, unsigned long offset,
-		    uint64_t value, memory_order order, int width)
-{
-	void *ptr = metal_io_virt(io, offset);
-
-	if (io->ops.write)
-		(*io->ops.write)(io, offset, value, order, width);
-	else if (ptr && sizeof(atomic_uchar) == width)
-		atomic_store_explicit((atomic_uchar *)ptr, (unsigned char)value,
-				      order);
-	else if (ptr && sizeof(atomic_ushort) == width)
-		atomic_store_explicit((atomic_ushort *)ptr,
-				      (unsigned short)value, order);
-	else if (ptr && sizeof(atomic_uint) == width)
-		atomic_store_explicit((atomic_uint *)ptr, (unsigned int)value,
-				      order);
-	else if (ptr && sizeof(atomic_ulong) == width)
-		atomic_store_explicit((atomic_ulong *)ptr, (unsigned long)value,
-				      order);
-#ifndef NO_ATOMIC_64_SUPPORT
-	else if (ptr && sizeof(atomic_ullong) == width)
-		atomic_store_explicit((atomic_ullong *)ptr,
-				      (unsigned long long)value, order);
-#endif
-	else
-		metal_assert(0);
 }
 
 void metal_io_init(struct metal_io_region *io, void *virt,
--- lib/io.h
+++ lib/io.h
@@ -108,10 +108,77 @@ metal_phys_addr_t metal_io_phys(struct metal_io_region *io, unsigned long offset
 unsigned long metal_io_phys_to_offset(struct metal_io_region *io, metal_phys_addr_t phys);
 void *metal_io_phys_to_virt(struct metal_io_region *io, metal_phys_addr_t phys);
 metal_phys_addr_t metal_io_virt_to_phys(struct metal_io_region *io, void *virt);
-uint64_t metal_io_read(struct metal_io_region *io, unsigned long offset,
-		       memory_order order, int width);
-void metal_io_write(struct metal_io_region *io, unsigned long offset,
-		    uint64_t value, memory_order order, int width);
+
+/**
+ * @brief	Read a value from an I/O region.
+ * @param[in]	io	I/O region handle.
+ * @param[in]	offset	Offset into I/O region.
+ * @param[in]	order	Memory ordering.
+ * @param[in]	width	Width in bytes of datatype to read.  This must be 1, 2,
+ *			4, or 8, and a compile time constant for this function
+ *			to inline cleanly.
+ * @return	Value.
+ */
+static inline uint64_t metal_io_read(struct metal_io_region *io, unsigned long offset,
+		       memory_order order, int width)
+{
+	void *ptr = metal_io_virt(io, offset);
+
+	if (io->ops.read)
+		return (*io->ops.read)(io, offset, order, width);
+	else if (ptr && sizeof(atomic_uchar) == width)
+		return atomic_load_explicit((atomic_uchar *)ptr, order);
+	else if (ptr && sizeof(atomic_ushort) == width)
+		return atomic_load_explicit((atomic_ushort *)ptr, order);
+	else if (ptr && sizeof(atomic_uint) == width)
+		return atomic_load_explicit((atomic_uint *)ptr, order);
+	else if (ptr && sizeof(atomic_ulong) == width)
+		return atomic_load_explicit((atomic_ulong *)ptr, order);
+#ifndef NO_ATOMIC_64_SUPPORT
+	else if (ptr && sizeof(atomic_ullong) == width)
+		return atomic_load_explicit((atomic_ullong *)ptr, order);
+#endif
+	metal_assert(0);
+	return 0; /* quiet compiler */
+}
+
+/**
+ * @brief	Write a value into an I/O region.
+ * @param[in]	io	I/O region handle.
+ * @param[in]	offset	Offset into I/O region.
+ * @param[in]	value	Value to write.
+ * @param[in]	order	Memory ordering.
+ * @param[in]	width	Width in bytes of datatype to read.  This must be 1, 2,
+ *			4, or 8, and a compile time constant for this function
+ *			to inline cleanly.
+ */
+static inline void metal_io_write(struct metal_io_region *io, unsigned long offset,
+		    uint64_t value, memory_order order, int width)
+{
+	void *ptr = metal_io_virt(io, offset);
+
+	if (io->ops.write)
+		(*io->ops.write)(io, offset, value, order, width);
+	else if (ptr && sizeof(atomic_uchar) == width)
+		atomic_store_explicit((atomic_uchar *)ptr, (unsigned char)value,
+				      order);
+	else if (ptr && sizeof(atomic_ushort) == width)
+		atomic_store_explicit((atomic_ushort *)ptr,
+				      (unsigned short)value, order);
+	else if (ptr && sizeof(atomic_uint) == width)
+		atomic_store_explicit((atomic_uint *)ptr, (unsigned int)value,
+				      order);
+	else if (ptr && sizeof(atomic_ulong) == width)
+		atomic_store_explicit((atomic_ulong *)ptr, (unsigned long)value,
+				      order);
+#ifndef NO_ATOMIC_64_SUPPORT
+	else if (ptr && sizeof(atomic_ullong) == width)
+		atomic_store_explicit((atomic_ullong *)ptr,
+				      (unsigned long long)value, order);
+#endif
+	else
+		metal_assert(0);
+}
 
 #define metal_io_read8_explicit(_io, _ofs, _order)			\
 	metal_io_read((_io), (_ofs), (_order), 1)
